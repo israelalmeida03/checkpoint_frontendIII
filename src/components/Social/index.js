f362f7536867026e79241dd67d17fe86a73536ab
israelalmeida03 <israelalmeida31@gmail.com>
2022-04-05 04:42:51 -0300

    Salvando no git

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -7,7 +7,7 @@ export default function Social(){
         <div className="social">
             <a href="https://www.instagram.com/"><FiInstagram size={30}/>  </a>
             <a href="https://www.linkedin.com/in/israel-almeida-6b914856/"><BsLinkedin size={30}/></a>
-            <a href="https://github.com/"> <BsGithub size={30}/></a>
+            <a href="https://github.com/israelalmeida03/checkpoint_frontendIII"> <BsGithub size={30}/></a>
         </div>
     )
 }
